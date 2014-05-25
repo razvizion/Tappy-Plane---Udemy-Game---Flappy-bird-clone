a01b828d8b44dc8b703727f87ee5c80bcbd2492c
razvizion <kolo111@gmail.com>
2014-05-25 14:11:59 +0200

    Graphics

---
+++ Tappy Plane/TPGameOverMenu.h
@@ -0,0 +1,13 @@
+//
+//  TPGameOverMenu.h
+//  Tappy Plane
+//
+//  Created by Michał Kozak on 25.05.2014.
+//  Copyright (c) 2014 Raz Labs. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface TPGameOverMenu : SKNode
+
+@end
---
+++ Tappy Plane/TPGameOverMenu.m
@@ -0,0 +1,13 @@
+//
+//  TPGameOverMenu.m
+//  Tappy Plane
+//
+//  Created by Michał Kozak on 25.05.2014.
+//  Copyright (c) 2014 Raz Labs. All rights reserved.
+//
+
+#import "TPGameOverMenu.h"
+
+@implementation TPGameOverMenu
+
+@end
